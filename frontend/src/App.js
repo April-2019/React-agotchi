8c3018a8685b92b214961a3b918b891a6bc125c0
shawn0527 <shawnren527@gmail.com>
2019-06-13 14:14:10 -0500

    create front end

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Login from './components/Login'
+
+export default class App extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return(
+            <div>
+                <img></img>
+                <Login />
+            </div>
+        )
+    }
+}
