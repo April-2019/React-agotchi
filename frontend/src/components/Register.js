9b27c2bef50d4620270ea2da011fd906b22b5e1e
shawn0527 <shawnren527@gmail.com>
2019-06-16 09:44:57 -0500

    0616 0945

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,27 +1,50 @@
 import React from 'react'
-import {Form, Container, Col, Button} from 'react-bootstrap'
+import {Form, Container, Col, Button, Row} from 'react-bootstrap'
 
 const Register = (props) => {
   return (
     <Container>
-      <Form>
-        <Form.Group controlId='formRegisterUsername'>
-          <Form.Label>Username</Form.Label>
-          <Form.Control type='text' placeholder='Enter Username'></Form.Control>
-          <Form.Text>Please enter your username here</Form.Text>
-        </Form.Group>
-        <Form.Group controlId='formRegisterPassword'>
-          <Form.Label>Password</Form.Label>
-          <Form.Control type='password' placeholder='Enter Password'></Form.Control>
-          <Form.Text>Please enter your password here</Form.Text>
-        </Form.Group>
-        <Form.Group controlId='formRegisterReenterPassword'>
-          <Form.Label>Re-enter Password</Form.Label>
-          <Form.Control type='password' placeholder='Re-enter Password'></Form.Control>
-          <Form.Text>Please Re-enter your password here</Form.Text>
-        </Form.Group>
-        <Button variant="outline-primary" type='submit'>Register</Button>
-        <Button variant="outline-secondary" onClick={() => props.goBack()}>Back To Login</Button>
+      <Form onSubmit={(e) => this.props.login(e)}>
+        <div className='register_background'>
+          <Row className="justify-content-center">
+            <Form.Group
+              as={Col}
+              xs='auto'
+              sm='auto'
+              md='auto'
+              lg='auto'
+              controlId='formLoginUsername'>
+              <Form.Label>Username</Form.Label>
+              <Form.Control type='text' placeholder='Enter Username' name="userName"/>
+              <Form.Text>Please enter your username here</Form.Text>
+            </Form.Group>
+          </Row>
+          <Row className="justify-content-center">
+            <Form.Group
+              as={Col}
+              xs='auto'
+              sm='auto'
+              md='auto'
+              lg='auto'
+              xl='auto'
+              controlId='formLoginPassword'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control type='password' placeholder='Enter Password'></Form.Control>
+              <Form.Text>Please enter your password here</Form.Text>
+            </Form.Group>
+          </Row>
+          <Row className="justify-content-center">
+            <Form.Group controlId='formRegisterReenterPassword'>
+              <Form.Label>Re-enter Password</Form.Label>
+              <Form.Control type='password' placeholder='Re-enter Password'></Form.Control>
+              <Form.Text>Please Re-enter your password here</Form.Text>
+            </Form.Group>
+          </Row>
+          <Row className="justify-content-center">
+          <Button variant="outline-primary" type='submit'>Register</Button>
+          <Button variant="outline-secondary" onClick={() => props.goBack()}>Back To Login</Button>
+          </Row>
+        </div>
       </Form>
     </Container>
   )
