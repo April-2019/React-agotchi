24684cffa0b620dcea26b6dceb2c4dc91a5565e0
Nick-2013 <nick-2013@live.com>
2019-06-19 11:31:26 -0500

    SpookyScarySkeletons

--- frontend/src/components/Grave.js
+++ frontend/src/components/Grave.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import {Form, Container, Col, Button, Row} from 'react-bootstrap'
+import './some.css'
+
 
 export default class Grave extends React.Component {
   render() {
     return (
-      <div style={{"background":"white","width":"200px"}}>
+      <div className="gravecard">
+        <div style={{"background":"grey","width":"200px"}}>
         <h2>{this.props.pet.name}</h2>
         <b>Age:</b> {this.props.pet.age}<br />
-        <b>Happiness:</b> {this.props.pet.happiness}<br />
-        <b>Fullness:</b> {this.props.pet.hunger}<br />
+        {/* <b>Happiness:</b> {this.props.pet.happiness}<br /> */}
+        {/* <b>Fullness:</b> {this.props.pet.hunger}<br /> */}
         <b>Type:</b> {this.props.pet.type}<br />
         <b>Stage:</b> {this.props.pet.stage}<br />
-        <b>Epitaph:</b> {this.props.pet.epitaph}
+        <b></b> Here lies {this.props.pet.name} the {this.props.pet.type}, they will be missed
+      </div>
       </div>
     );
   }
